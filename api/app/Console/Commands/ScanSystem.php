93c6752d1579ed9d35b4489bd9b5a4f2fcf43a7b
Bob Arezina <bob.arezina@intellipharm.com.au>
2020-11-15 11:22:15 +1000

    Place an ngram-3 filter in so you can do partial filename matches

--- api/app/Console/Commands/ScanSystem.php
+++ api/app/Console/Commands/ScanSystem.php
@@ -23,7 +23,7 @@ class ScanSystem extends Command
     {
         $this->info('Blazefind v1.0.0');
         $this->info('Scanning system for files...');
-        // system("find /host/code -printf \"%T@|%s|%h|%f\n\" > files.txt");
+        system("find /host -printf \"%T@|%s|%h|%f\n\" > files.txt");
 
         // connect to elastic
         $hosts = ['elasticsearch:9200'];
@@ -44,40 +44,77 @@ class ScanSystem extends Command
 
         if ($indexExists) {
             $this->info('index exists');
-        } else {
-            $this->info('index doesnt exist');
 
-            $params = [
-                'index' => $elasticIndexName . '-000001',
-                'body' => [
-                    'mappings' => [
-                        'properties' => [
-                            'created_at' => ['type' => 'date'],
-                            'size' => ['type' => 'long'],
-                            'path' => ['type' => 'text'],
-                            'filename' => ['type' => 'text']
-                        ]
-                    ]
-                ]
+            $deleteParams = [
+                'index' => $elasticIndexName . '-000001'
             ];
 
-            $response = $client->indices()->create($params);
+            $response = $client->indices()->delete($deleteParams);
 
-            $params = [
-                'body' => [
-                    'actions' => [
-                        [
-                            'add' => [
-                                'index' => $elasticIndexName . '-000001',
-                                'alias' => $elasticIndexName
+        }
+
+        $this->info('index doesnt exist');
+
+        $params = [
+            'index' => $elasticIndexName . '-000001',
+            'body' => [
+                'mappings' => [
+                    'properties' => [
+                        'id' => ['type' => 'text'],
+                        'created_at' => ['type' => 'date'],
+                        'size' => ['type' => 'long'],
+                        'path' => ['type' => 'text'],
+                        'filename' => [
+                            'type' => 'text',
+                            'fields' => [
+                                'ngram' => [
+                                    'type' => 'text',
+                                    'analyzer' => 'my_analyzer'
+                                ]
+                            ]
+                        ]
+                    ]
+                ],
+                'settings' => [
+                    'analysis' => [
+                        'analyzer' => [
+                            'my_analyzer' => [
+                                'tokenizer' => 'my_tokenizer'
+                            ]
+                        ],
+                        'tokenizer' => [
+                            'my_tokenizer' => [
+                                'type' => 'ngram',
+                                'min_gram' => 3,
+                                'max_gram' => 3,
+                                'token_chars' => [
+                                    'letter',
+                                    'digit'
+                                ]
                             ]
                         ]
                     ]
                 ]
-            ];
+            ]
+        ];
 
-            $response = $client->indices()->updateAliases($params);
-        }
+        $response = $client->indices()->create($params);
+
+        $params = [
+            'body' => [
+                'actions' => [
+                    [
+                        'add' => [
+                            'index' => $elasticIndexName . '-000001',
+                            'alias' => $elasticIndexName
+                        ]
+                    ]
+                ]
+            ]
+        ];
+
+        $response = $client->indices()->updateAliases($params);
+
 
         $reader = Reader::createFromPath('files.txt', 'r');
         $reader->setDelimiter('|');
@@ -86,6 +123,7 @@ class ScanSystem extends Command
         $records = $stmt->process($reader);
 
         $params = ['body' => []];
+        $counter = 1;
 
         foreach ($records as $record) {
 
@@ -93,30 +131,47 @@ class ScanSystem extends Command
             $createdAt = Carbon::createFromTimestamp($unixWithoutMicro);
             $size = $record[1];
             $path = $record[2];
+            $path = str_replace('/host/', '/', $path);
             $filename = $record[3];
+
+            $id = $path . '/' . $filename;
 
             $this->info("$unixWithoutMicro $size $path $filename");
 
             $params['body'][] = [
                 'index' => [
                     '_index' => $elasticIndexName,
+                    '_id'    => $id
                 ]
             ];
 
             $params['body'][] = [
-                'created_at' => $unixWithoutMicro,
+                'created_at' => $createdAt,
                 'size' => $size,
                 'path' => $path,
                 'filename' => $filename
             ];
 
-            break;
+            // Every 1000 documents stop and send the bulk request
+            if ($counter % 1000 == 0) {
+                $this->info('sending 1000...');
+                $responses = $client->bulk($params);
+
+                // erase the old bulk request
+                $params = ['body' => []];
+
+                // unset the bulk response when you are done to save memory
+                unset($responses);
+
+                $counter = 1;
+            } else {
+                $counter++;
+            }
         }
 
-
-        $response = $client->bulk($params);
-        print_r($response);
-
-
+        // Send the last batch if it exists
+        if (!empty($params['body'])) {
+            $responses = $client->bulk($params);
+        }
     }
 }
